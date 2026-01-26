e7b470eb9113498ffafa1611bb8bc21ef91d34d5
TyBouc <boucino.ty@gmail.com>
2026-01-26 13:38:01 -0500

    lots of changes, fixed some stuff with the scrollable, added a featured product

--- js/index.js
+++ js/index.js
@@ -12,10 +12,19 @@ async function loadProducts() {
         // Loop through dta and create HTML
 
         data.forEach(product => {
+            // Link Creation
+            const link = document.createElement('a');
+            link.href = product.link || '#';  // Use product.link from your sheet
+            link.target = '_blank';           // opens in new tab
+            link.classList.add('featured-link');
+            link.style.textDecoration = 'none';
+            link.style.color = 'inherit';
+
+            // Card Creation
             const card = document.createElement('div');
             card.classList.add('featured-card');
 
-            // We match the keys EXACTLY to your JSON: company, name, price, image
+            // We match the keys EXACTLY to JSON: company, name, price, image
             card.innerHTML = `
                 <img src="${product.image}" alt="${product.name}">
                 <div class="card-content" style="padding: 10px;">
@@ -24,7 +33,8 @@ async function loadProducts() {
                     <p style="font-weight: bold; margin: 0;">$${product.price}</p>
                 </div>
             `;
-            featuredList.appendChild(card);
+            featuredList.appendChild(link);
+            link.appendChild(card);
         });
     } catch (error) {
         console.error('Error fetching products:', error);
